7bf4bc464a29263d4c5bdd391ce9cb85b66280e3
MaverickUKR <mazepav88@gmail.com>
2024-04-08 18:29:25 +0300

    Changed: video play section

--- src/Components/VideoSection/VideoSection.jsx
+++ src/Components/VideoSection/VideoSection.jsx
@@ -5,7 +5,13 @@ const VideoSection = () => {
   return (
     <main style={{ width: "100%", height: "100%" }} id="Home">
       <div className="video__container">
-        <video width="100%" height="100%" loop autoPlay>
+        <video
+          width="100%"
+          height="100%"
+          loop
+          autoplay="autoplay"
+          id="autoplay"
+        >
           <source src={presentVideo} type="video/mp4" />
         </video>
       </div>
